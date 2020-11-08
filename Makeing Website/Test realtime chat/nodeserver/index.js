e36aec68c3c42da6fed7f8082faae0a4b51177fd
Kapil Singh Chauhan <kapilchauhan9423@gmail.com>
2020-11-08 11:27:11 +0530

    it the 08-11-2020 i am lerning javascript at this time

---
+++ Makeing Website/Test realtime chat/nodeserver/index.js
@@ -0,0 +1,17 @@
+//NOde srver which will handle socet io connections
+
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        console.log('new user', name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+})
